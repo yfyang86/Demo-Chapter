7ead2affeb9f2530f95ea74b21e93f9bd7d32173
Yifan <siiowl@users.noreply.github.com>
2014-05-18 15:54:46 -0400

    Update char_Split.R

--- char_Split.R
+++ char_Split.R
@@ -1,8 +1,8 @@
 ## @brief:  return TRUE if there were 
 #           any number showing up more than 
-#           3 times (i.e. >=4) continuously.
+#           lim=4 times (i.e. >=5) continuously.
 
-string.counting<-Vectorize(function(txt,lim=4){#Yifan
+string.counting<-Vectorize(function(txt,lim=5){#Yifan
   #str_match(txt,"([0-9]+)")
   xx=strsplit(txt,split='')[[1]]
   yy=as.numeric(xx)
@@ -12,16 +12,17 @@ string.counting<-Vectorize(function(txt,lim=4){#Yifan
   char=length(xx)-nu
   xx.numericpart[!xx.nan]=sample(-(1:10000),char)
   flag=F
+  tg.chars=NA
   if (length(xx.numericpart[xx.nan])>lim){
     for (i in 1:(length(xx.numericpart)-lim+1)){
       tg=xx.numericpart[i];
-      if (sum(xx.numericpart[i:(i+3)]==tg)==4) {flag=T;break;}
+      if (sum(xx.numericpart[i:(i+lim-1)]==tg)==lim) {tg.chars=tg;flag=T;break;}
     }
   }
- 
-  return(list(length=nu+char,nu.len=nu,char.len=char,flags=flag,numeric=yy));
+  
+  return(list(length=nu+char,nu.len=nu,char.len=char,flags=flag,numeric=yy,tg=tg.chars));
 })
- 
- 
-a=c("absadj102220aid000998","10000ans")
+
+
+a=c("absadj102220aid000999998","10000ans",'ABC')
 string.counting(a)
