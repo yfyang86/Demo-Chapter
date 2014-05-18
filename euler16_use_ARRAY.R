c8c73f781f0a8ca1a72620f73954a9ba4d1aa2ec
Yifan <siiowl@users.noreply.github.com>
2014-05-18 14:35:04 -0400

    Create euler16_use_ARRAY.R

---
+++ euler16_use_ARRAY.R
@@ -0,0 +1,25 @@
+# 
+# @brief LONG INT calculation
+# Power digit sum
+# Problem 16
+# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# What is the sum of the digits of the number 21000?
+# @para: n 2^n
+
+f16L <- function(n){
+  if (!is.integer(n)) stop("must be int");
+  if (n<=0) stop('must be >0')
+  if (n>3318) stop('Temp: n<=3318')
+  dig.num=rep(0L,1000)
+  digits <- as.integer(unlist(strsplit(as.character(2), split = "")))
+  dig.num[1] <- rev(digits)
+  for (i in 1:(n-1)){
+    dig2 <- dig.num *2
+    dig3 <- dig2 %%10
+    dig4 <- floor(dig2/10)
+    dig.num <- dig3 + c(0,dig4[-302])
+  }  
+  sum(dig.num)
+}
+
+# f16L(1000L)
