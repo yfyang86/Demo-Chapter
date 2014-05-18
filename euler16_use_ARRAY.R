483c090ef6eda393db7b53baa3517ac2627872da
Yifan <siiowl@users.noreply.github.com>
2014-05-18 14:59:57 -0400

    2^3320 vs 10^1000

--- euler16_use_ARRAY.R
+++ euler16_use_ARRAY.R
@@ -10,7 +10,7 @@
 f16L <- Vectorize(function(n){
   if (!is.integer(n)) stop("must be int");
   if (n<=0) stop('must be >0')
-  if (n>3318) stop('Temp: n<=3318')
+  if (n>3320) stop('Temp: n<=3320')
   dig.num=rep(0L,1000)
   # 38: rev(digits)
   digits <- as.integer(unlist(strsplit(as.character(2), split = "")))
@@ -19,7 +19,7 @@ f16L <- Vectorize(function(n){
     dig2 <- dig.num *2
     dig3 <- dig2 %%10
     dig4 <- floor(dig2/10)
-    dig.num <- dig3 + c(0,dig4[-302])
+    dig.num <- dig3 + c(0,dig4[-1000])
   }  
   sum(dig.num)
 })
