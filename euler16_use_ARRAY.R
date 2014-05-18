53846ea79a2aef303f8140a909dc374bba424ecb
Yifan <siiowl@users.noreply.github.com>
2014-05-18 14:37:13 -0400

    Vectorized

--- euler16_use_ARRAY.R
+++ euler16_use_ARRAY.R
@@ -6,11 +6,13 @@
 # What is the sum of the digits of the number 21000?
 # @para: n 2^n
 
-f16L <- function(n){
+
+f16L <- Vectorize(function(n){
   if (!is.integer(n)) stop("must be int");
   if (n<=0) stop('must be >0')
   if (n>3318) stop('Temp: n<=3318')
   dig.num=rep(0L,1000)
+  # 38: rev(digits)
   digits <- as.integer(unlist(strsplit(as.character(2), split = "")))
   dig.num[1] <- rev(digits)
   for (i in 1:(n-1)){
@@ -20,6 +22,6 @@ f16L <- function(n){
     dig.num <- dig3 + c(0,dig4[-302])
   }  
   sum(dig.num)
-}
+})
 
-# f16L(1000L)
+#f16L(c(25L,1000L))
