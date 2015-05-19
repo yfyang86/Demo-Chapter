8a900b3bda96f29a851288dc5b2c5cf8548a5d42
Yifan <yfyang86@users.noreply.github.com>
2015-05-18 22:13:46 -0400

    BED read plain version

--- src/bedREAD.cpp
+++ src/bedREAD.cpp
@@ -1,10 +1,49 @@
+/*
+ Hints:
+ 
+ 1.
+ 
+ offset
+ 
+ The type std::streamoff is a signed integral type of sufficient size to represent 
+ the maximum possible file size supported by the operating system. 
+ Typically, this is a typedef to (singned) long long.
+ 
+ Hence it has no problem
+ 
+ 2.
+ 
+ There is no guanrantee that `unsigned char` is 8 bits. Hence I use a trick to check that:
+ 
+ int countbits(){
+    char c;
+    int bits=0;
+    for ( c = 1; c; c <<= 1, bits++ );
+    return bits;
+}
+
+will counts the bits, then
+
+use bits operation within each 
+
+while ( ifsream.get(char x)  && endcondition){
+
+for (i in 1:(bits/8)){
+  do sth
+  bit = x>>8;
+  }
+ }
+ 
+ */
+
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <stdint.h>
 #include <climits>
-#include <bitset>
+//bitset: used to print
+//#include <bitset>
 
 
 inline long N4(long n){
@@ -23,22 +62,20 @@ int countbits(){
     return bits;
 }
 
+
+//usage: bedread(string befile, long int smaple_size, long int VBlock, long int id)
+
 std::vector<uint8_t> bedread(
   std::string bedfilename,
   long int N,
   long int VBlock,
   long int id
 ){
-  
-  std::ofstream ofs("ll.txt");
-  
-  
   uint8_t opz;
   char opz2;
-  std::bitset<8> xx;
+  //std::bitset<8> xx;
   int scale = countbits()/8;
-  uint8_t lable8[4]={192, 48, 12, 3};
-  //{0x11000000,0x00110000,0x00001100,0x00000011};// 192 48 12 3
+  uint8_t lable8[4]={0xC0,0x30,0xC,0x3};// 192 48 12 3
   uint64_t tmpN4 = (uint64_t)N4(N);
   uint64_t PSG = 2 + tmpN4 * (uint64_t)id; // Magic = 3, off = 3-1
   std::vector<uint8_t> re;
@@ -46,44 +83,48 @@ std::vector<uint8_t> bedread(
     std::cout<<"Overflow";
   }else{
     re.resize(N);
+    /* debug: file information
     std::cout<<bedfilename<<std::endl;
     std::cout<<"Len "<<tmpN4<<"\toffset "<<PSG<<"\t Scale: "<<scale<<std::endl;
+    */
     std::ifstream uuid(bedfilename.c_str(),std::ios::in | std::ios::binary);
-    // seek
+    // seek PSG offset
     uuid.seekg(PSG,std::ios::cur);
     long int ii=0;
     if (uuid.is_open()){
-      for (int i=0;i<tmpN4;i++){
-	//uuid.read((char*)&opz,sizeof(uint8_t));
-	uuid.get(opz2);
+      int i=0;
+      while(i<tmpN4 && uuid.get(opz2)){
 	for (int ij=0;ij<scale;ij++){
-	  opz = opz2;
-	  opz2>>8;
+	  opz = opz2; 		// lower 8 bits
+	  opz2 = opz2>>8; 	// move to right by 8
+	  //xx=opz; 		// for print
+	  //std::cout<<"log-"<<i<<"\t"<<xx<<"\n";
+	  for (int j=0;j<=3;j++) {
+	    re[ii++] = (opz & lable8[j]);
+	    re[ii-1] = re[ii-1] >> 2*(3-j);
+	    if ( ii >= N){
+	      goto outforloop;// I need two breaks...
+	    }
+	  }
+	
 	  i++;
-	}
-	if (opz>0) std::cout<<"*";
-	xx=opz;
-	std::cout<<"log-"<<i<<"\t"<<xx<<"\n";
-	if ( ii < N){
-	  for (int j=0;j<3;j++) re[ii++] = (opz&lable8[j]);
-	}else{
-	  break;
 	}
       }
     }else{
       std::cout<<"File no exist\n";
     }
-for(int o=0;o<N;o++) ofs<re[o]<<",";
-    uuid.close();
+outforloop: uuid.close();
   }
   return re;
 }
 
-/*
+
+// id start with 0
+
 int main(int argc, char **argv) {
     std::string file1("/home/yifan/Downloads/ALL.SNP.EUR.chr2.phase3_v5.20130502.genotypes.bed");
-    std::vector<uint8_t> result1 = bedread(file1,100,10,0);
-    std::cout<<"--re--\n"<<unsigned(result1[0])<<" "<<unsigned(result1[1])<<std::endl;
+    std::vector<uint8_t> result1 = bedread(file1,17,10,0);
+    std::cout<<"--re--\n";
+    for (int i=0;i<10;i++ ) std::cout<<"Sample "<<i<<" val:"<<+result1[i]<<std::endl;
     return 0;
 }
-*/
