c92bb6e0f54dfd0078324cad6b37e9be698a1580
Yifan <yfyang86@users.noreply.github.com>
2015-08-12 16:52:14 -0400

    Correction: add blank pdf

--- src/renumber.sh
+++ src/renumber.sh
@@ -9,8 +9,22 @@ else
     echo "\n">>renumTemp.tex
     echo "\\\begin{document}">>renumTemp.tex
     u=`pdfinfo $1 | grep "Pages" | awk '{print $2}'`
+    s=$(($(($u / 8)) + 1))
+    s=$(($(($s * 8)) - $u))
+    if [ $s -gt 0 ];then
+	echo "=============="
+	echo "Add $s Pages"
+	echo "=============="
+	echo "" | ps2pdf -sPAPERSIZE=letter - blank.pdf
+	ss=1
+	while [ $ss -le $s ];do
+		pdftk A=$1 B=blank.pdf cat A1-end B1 output temp9375.pdf
+		mv temp9375.pdf $1
+		ss=$(($ss + 1))
+	done
+    fi
+    u=$(($u + $s))
     starter=1
-    if [ $u -gt 4 ]; then
 	while [ $starter -lt $u ]; do
 		s1=$(($starter+2))
 		s2=$(($starter+4))
@@ -23,13 +37,7 @@ else
 		echo "\\includepdf[pages={$u1,$u2,$u3,$u4}]{$1}">>renumTemp.tex
 		starter=$(($starter+8))
 	done
-    fi
-#remain
-while [ $starter -le $u ]; do
-	echo "\includepdf[pages=$starter]{$1}">>renumTemp.tex
-	starter=$(($starter+1))
-done;
 echo "\end{document}">>renumTemp.tex
 pdflatex renumTemp.tex
-rm renumTemp.tex
+rm renumTemp.tex blank.pdf
 fi
